0aad28398023a0952d53ddc5614c02f160766550
peerajak <peerajak@gmail.com>
2024-10-29 02:52:35 +0000

    add unit 10 3d visualization for robot

---
+++ unit_10/touch_files.sh
@@ -0,0 +1,5 @@
+touch ~/webpage_ws/unit_10/libs/ColladaLoader.js
+touch ~/webpage_ws/unit_10/libs/ColladaLoader2.js
+touch ~/webpage_ws/unit_10/libs/ros3d.min.js
+touch ~/webpage_ws/unit_10/libs/STLLoader.js
+touch ~/webpage_ws/unit_10/libs/three.min.js
